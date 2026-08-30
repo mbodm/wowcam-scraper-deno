632e1a5b56aee1dba6dc135cd8ad1d98c8df392f
MBODM <webmaster@mbodm.com>
2026-08-30 13:26:55 +0200

    Fixed main.ts file

--- main.ts
+++ main.ts
@@ -2,27 +2,28 @@ import { startServer } from "@/api/server.ts";
 
 const server = startServer(8000);
 
-Deno.addSignalListener("SIGTERM", () => exitGracefully("SIGTERM"));
-Deno.addSignalListener("SIGINT", () => exitGracefully("SIGINT"));
+const onSigterm = () => exitGracefully("SIGTERM");
+const onSigint = () => exitGracefully("SIGINT");
 
-function exitGracefully(signal: string): void {
+Deno.addSignalListener("SIGTERM", onSigterm);
+Deno.addSignalListener("SIGINT", onSigint);
+
+async function exitGracefully(signal: string): Promise<void> {
   console.log(`Closing server now, because a ${signal} signal was received.`);
   // Force exit if server.shutdown() hangs
-  const cb = () => {
+  const forceExitTimer = setTimeout(() => {
     console.error("Forcing exit, because a timeout occurred while closing the server.");
     Deno.exit(1);
-  };
-  const delay = 10 * 1000;
-  const forceExitTimer = setTimeout(cb, delay);
-  server.shutdown()
-    .then(() => {
-      clearTimeout(forceExitTimer);
-      console.log("Server successfully closed.");
-      Deno.exit(0);
-    })
-    .catch((err) => {
-      clearTimeout(forceExitTimer);
-      console.error("Forcing exit, because an error occurred while closing the server:", err);
-      Deno.exit(1);
-    });
+  }, 10 * 1000);
+  try {
+    await server.shutdown();
+    clearTimeout(forceExitTimer);
+    Deno.removeSignalListener("SIGTERM", onSigterm);
+    Deno.removeSignalListener("SIGINT", onSigint);
+    console.log("Server successfully closed.");
+  } catch (err) {
+    clearTimeout(forceExitTimer);
+    console.error("Forcing exit, because an error occurred while closing the server:", err);
+    Deno.exit(1);
+  }
 }
