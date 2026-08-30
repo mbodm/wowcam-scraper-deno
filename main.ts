c8aa07d2da083dd3ec56196435f9e03f0c50aef6
MBODM <webmaster@mbodm.com>
2026-08-30 13:06:20 +0200

    Fixed main.ts english and adjusted code a tiny bit

--- main.ts
+++ main.ts
@@ -6,14 +6,14 @@ Deno.addSignalListener("SIGTERM", () => exitGracefully("SIGTERM"));
 Deno.addSignalListener("SIGINT", () => exitGracefully("SIGINT"));
 
 function exitGracefully(signal: string): void {
-  console.log(`Closing server now, because received ${signal} signal.`);
+  console.log(`Closing server now, because a ${signal} signal was received.`);
   // Force exit if server.shutdown() hangs
-  const forceExitTimer = setTimeout(() => {
-    console.error(
-      "Forcing exit, because a timeout occurred while closing server.",
-    );
+  const cb = () => {
+    console.error("Forcing exit, because a timeout occurred while closing the server.");
     Deno.exit(1);
-  }, 10 * 1000);
+  };
+  const delay = 10 * 1000;
+  const forceExitTimer = setTimeout(cb, delay);
   server.shutdown()
     .then(() => {
       clearTimeout(forceExitTimer);
@@ -22,10 +22,7 @@ function exitGracefully(signal: string): void {
     })
     .catch((err) => {
       clearTimeout(forceExitTimer);
-      console.error(
-        "Forcing exit, because an error occurred while closing server:",
-        err,
-      );
+      console.error("Forcing exit, because an error occurred while closing the server:", err);
       Deno.exit(1);
     });
 }
